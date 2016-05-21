95a0360f5b533d18467839f87dbb58826e8e1704
Edson Hilios <edson.hilios@unear.net>
2016-05-20 21:56:46 -0300

    Add more predicates

--- wumpus.pl
+++ wumpus.pl
@@ -23,22 +23,22 @@ world(4, 4).
 %     +---+---+---+---+
 %   4 |   |   |   | P |
 %     +---+---+---+---+
-%   3 | W |   | P |   |
+%   3 | W | G | P |   |
 %     +---+---+---+---+
 %   2 |   |   |   |   |
 %     +---+---+---+---+
-%   1 | H | G | P |   |
+%   1 | H |   | P |   |
 %     +---+---+---+---+
 %       1   2   3   4
-
-% The initial database.
+% Database.
 % hunter(1, 1, east).
 % wumpus(1, 3, alive).
 % pit(3, 1).
 % pit(3, 3).
 % pit(4, 4).
-% gold(2, 1).
+% gold(2, 3).
 
+% Test database
 %     +---+---+---+---+
 %   4 |   |   |   | G |
 %     +---+---+---+---+
@@ -49,8 +49,6 @@ world(4, 4).
 %   1 | H |   |   |   |
 %     +---+---+---+---+
 %       1   2   3   4
-
-% The initial database.
 hunter(1, 1, east).
 gold(4, 4).
 
@@ -117,7 +115,7 @@ direction(west,  180).
 direction(south, 270).
 
 % Check if position is into map bounds.
-is_in_bounds(X, Y) :-
+in_bounds(X, Y) :-
   world(W, H),
   X > 0, X =< W,
   Y > 0, Y =< H.
@@ -125,14 +123,14 @@ is_in_bounds(X, Y) :-
 % Moves the Player to a new position.
 move(X, Y) :-
   assertz(actions(move)),
-  is_in_bounds(X, Y),
+  in_bounds(X, Y),
   format("- Moving to ~dx~d~n", [X,Y]),
   assertz( visited(X, Y) ),
   hunter(_, _, D),
   retractall( hunter(_, _, D) ), % Reset the hunter pos then reassign.
   asserta( hunter(X, Y, D) ),
   !.
-move(X, Y) :- format('Cannot move to ~dx~d~n', [X, Y]).
+move(X, Y) :- format('- Cannot move to ~dx~d~n', [X, Y]).
 
 % Shoot at position and kill wumpus if its there
 shoot(X, Y) :-
@@ -144,7 +142,20 @@ shoot(X, Y) :-
   !.
 shoot(_, _) :- write('I don not have arrows anymore.').
 
-% Player's actions.
+% Get the position ahead of the player
+next(X, Y) :- hunter(Xi, Yi, east),  X is Xi+1, Y is Yi, !.
+next(X, Y) :- hunter(Xi, Yi, north), X is Xi, Y is Yi+1, !.
+next(X, Y) :- hunter(Xi, Yi, west),  X is Xi-1, Y is Yi, !.
+next(X, Y) :- hunter(Xi, Yi, south), X is Xi, Y is Yi-1, !.
+
+% Get all neighbors
+neighbors(L) :- findall(N, neighbor(N), L).
+neighbor(B) :- hunter(X, Y, _), E is X+1, in_bounds(E, Y), B = [E, Y].
+neighbor(B) :- hunter(X, Y, _), N is Y+1, in_bounds(X, N), B = [X, N].
+neighbor(B) :- hunter(X, Y, _), W is X-1, in_bounds(W, Y), B = [W, Y].
+neighbor(B) :- hunter(X, Y, _), S is Y-1, in_bounds(X, S), B = [X, S].
+
+% Player's actions
 action(grab) :-
   assertz(actions(grab)),
   hunter(X, Y, _), assertz( grab(X, Y) ), \+ gold(X, Y),
@@ -175,17 +186,17 @@ action(turnright) :-
   retractall( hunter(_, _, _) ),
   asserta( hunter(X, Y, D) ).
 
-action(forward) :- hunter(X, Y, east),  E is X + 1, move(E, Y), !.
-action(forward) :- hunter(X, Y, north), N is Y + 1, move(X, N), !.
-action(forward) :- hunter(X, Y, west),  W is X - 1, move(W, Y), !.
-action(forward) :- hunter(X, Y, south), S is Y - 1, move(X, S), !.
+action(forward) :- next(X, Y), move(X, Y), !.
+action(forward) :- next(X, Y), move(X, Y), !.
+action(forward) :- next(X, Y), move(X, Y), !.
+action(forward) :- next(X, Y), move(X, Y), !.
 
-action(shoot) :- hunter(X, Y, east),  E is X + 1, shoot(E, Y), !.
-action(shoot) :- hunter(X, Y, north), N is Y + 1, shoot(X, N), !.
-action(shoot) :- hunter(X, Y, west),  W is X - 1, shoot(W, Y), !.
-action(shoot) :- hunter(X, Y, south), S is Y - 1, shoot(X, S), !.
+action(shoot) :- next(X, Y), shoot(X, Y), !.
+action(shoot) :- next(X, Y), shoot(X, Y), !.
+action(shoot) :- next(X, Y), shoot(X, Y), !.
+action(shoot) :- next(X, Y), shoot(X, Y), !.
 
-action(exit) :- write('Bye, bye...'), nl, halt.
+action(exit) :- write('- Bye, bye...'), nl, halt.
 
 % Apply a list of actions
 action([]).
@@ -220,32 +231,43 @@ has_wumpus(X, Y) :-
 % Define heuristics            %
 % ---------------------------- %
 heuristic(avoid_pit) :-
-  write('- Avoiding pit'), nl,
+  write('Avoiding pit. '),
   hunter(X, Y, _), assertz( breeze_at(X, Y) ).
 
 heuristic(avoid_wumpus) :-
-  write('- Avoiding wumpus'), nl,
+  write('Avoiding wumpus. '),
   hunter(X, Y, _), assertz( stench_at(X, Y) ).
 
-heuristic(get_back) :-
-  write('- Get back'), nl.
+heuristic(get_back) :- write('Get back. ').
 
 % perceptions([Stench, Breeze, Glitter, Bump, Scream])
-take_action([_, _, _, _, _],   exit)   :-
+take_action([_, _, _, _, _], exit)   :-
   hunter(1, 1, _), has_gold(yes), !.
 
+take_action([_, _, _, _, _],  A) :-
+  hunter(X, Y, _), has_gold(G), X \== 1, Y \== 1, G == gold,
+  heuristic(get_back),
+  A = forward,
+  !.
+
 take_action([_, _, yes, _, _], grab) :- !.
+
 take_action([_, _, _, yes, _], A) :-
   A = turnleft,
   !.
+
 take_action([_, yes, _, _, _], A) :-
   heuristic(avoid_pit),
   A = [turnleft, turnleft, forward],
   !.
 
-take_action([_, _, _, _, _],   forward)   :- !.
+take_action([_, _, _, _, _], forward)   :- !.
 
-% Run the game.
+% Print
+print_result :-
+  format('Score: ~d.', [0]).
+
+% Run the game
 run :- runloop(0).
 
 runloop(100) :- !.
